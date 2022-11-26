98152c6de7e52d573bbed606b3f5f844611f9263
dominic lizarraga <dominiclizarraga@hotmail.com>
2022-11-25 23:02:48 -0800

    added more to js controller but not work 100

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -9,18 +9,39 @@ export default class extends Controller {
   static values = {
     chatroomId: Number
   }
+  static values = { chatroomId: Number, currentUserId: Number }
+
 
   connect() {
     // this allows us to connect to rails websocket, it creates an instance
     // it needs one argument (the channel), in this case is the chatroom_channel
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
 
     this.channel = createConsumer().subscriptions.create(
         { channel: "ChatroomChannel", id: this.chatroomIdValue},
-        { received: (data) => { this.#insertMessage(data) } }
+        { received: data => this.#insertMessageAndScrollDown(data) }
+          console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
         )
+      }
 
-  }
+
+
+   #justifyClass(currentUserIsSender) {
+      return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+    }
+
+    #userStyleClass(currentUserIsSender) {
+      return currentUserIsSender ? "sender-style" : "receiver-style"
+    }
+
+    #buildMessageElement(currentUserIsSender, message) {
+      return `
+        <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+          <div class="${this.#userStyleClass(currentUserIsSender)}">
+            ${message}
+          </div>
+        </div>
+      `
+    }
 
     disconnect() {
       console.log("Unsubscribed from the chatroom")
@@ -31,10 +52,13 @@ export default class extends Controller {
       const form = event.target
       form.reset()
     }
-    // private
-    #insertMessage(data) {
-      this.messagesTarget.insertAdjacentHTML("beforeend", data)
-      // scroll to the bottom of the msgs list
+
+
+    #insertMessageAndScrollDown(data) {
+      const currentUserIsSender = this.currentUserIdValue === data.sender_id
+      const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+      this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
       this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
     }
-}
+
+  }
